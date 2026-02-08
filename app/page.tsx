79b1fbef658a59b5929d2a617dfdc45bb5101071
pero4l <lilpero03@gmail.com>
2026-02-08 05:44:15 +0100

    feat: implementing the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@
 
 export default function Home() {
   return (
-    <div className="">
-      <h1 className="text-3xl font-bold">Welcome to Ravo Wallet</h1>
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      {/* Logo */}
     </div>
   );
 }
